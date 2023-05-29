9777b5c2250cddbe8f89ecd36bbbe7d8d2075095
Rushikesh Radhakrishna Mhase <rushikesh.mhase@Yash.com>
2023-05-29 12:51:28 +0530

    Added Sevice Registry

--- product-cart/src/main/java/com/yash/ProductCartApplication.java
+++ product-cart/src/main/java/com/yash/ProductCartApplication.java
@@ -2,12 +2,14 @@ package com.yash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductCartApplication {
 
 	public static void main(String[] args) {
